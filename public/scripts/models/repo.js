3694de716212e7b9e677240be82b7ad2bfff4dd7
daniel <dpillay03@gmail.com>
2017-09-28 11:15:17 -0700

    possible token fix

--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -14,7 +14,7 @@ var app = app || {};
     $.get('/github/user/repos')
       .then(data => repos.all = data, err => console.error(err))
       .then(callback);
-  }
+  };
 
   repos.with = attr => repos.all.filter(repo => repo[attr]);
 
